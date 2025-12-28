e2356fdbe275ee71893605f2877391236f56ac13
WazupSteve <amit05.prakash@gmail.com>
2025-12-28 21:51:19 +0530

    p01:add test

--- tests/test_sdk.py
+++ tests/test_sdk.py
@@ -1,25 +1,25 @@
-import pytest
-from nano_modal.serialize import serialize_function, deserialize, serialize_args
+from nano_modal.serialize import deserialize, serialize_args, serialize_function
+
 
 def test_serial_deserial_workflow():
     # test function
-    def compute(x,y):
-        return x**2 + y**3 
+    def compute(x, y):
+        return x**2 + y**3
 
     # serializing the function
     fxn_bytes = serialize_function(compute)
 
-    # args,kwargs serializing 
-    args_bytes = serialize_args(4,y=3)
+    # args,kwargs serializing
+    args_bytes = serialize_args(4, y=3)
 
     # deserialize
     fxn_new = deserialize(fxn_bytes)
     args_new_tuple = deserialize(args_bytes)
 
-    #unpacking tuple
+    # unpacking tuple
     args_new, kwargs_new = args_new_tuple
 
-    #execute the new function with the new args
-    result = fxn_new(*args_new,**kwargs_new)
+    # execute the new function with the new args
+    result = fxn_new(*args_new, **kwargs_new)
 
-    assert result == compute(4,3)
+    assert result == compute(4, 3)
